9b0b75b8fcebe82460e2ef472ed9b84234bb9a7a
Eduard-Konovka <ed098ua@gmail.com>
2021-11-14 02:01:20 +0200

    Updatet index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
