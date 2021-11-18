6456b8c897edcc2e28bd73ea931b0f38c0ca0339
Eduard-Konovka <ed098ua@gmail.com>
2021-11-18 18:55:47 +0200

    Adds react-spinner-loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Loader from 'react-loader-spinner';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
 
 export default function App() {
   return (
     <div>
-      'Hello world!'
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
       <ToastContainer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import App from './App';
 
 ReactDOM.render(
